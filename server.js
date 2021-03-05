a3a4a3a9226a8a1e370ad66fb3c7cabdabea333a
Griffin Barlow <griffinbrlw@gmail.com>
2021-03-05 18:31:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -28,14 +28,12 @@ app.route('/api')
     const json = await data.json();
     res.json(json);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-
- // const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
- //  const json = await data.json();
- //   console.log('data from fetch', json);
- //   res.json(json);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    res.json(json);
   });
 
 app.listen(port, () => {
